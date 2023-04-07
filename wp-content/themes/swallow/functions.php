cebf4fe548687dbd2ac8aae7d407fecce5bba56d
MWinterE <mwexcellency@outlook.com>
2023-04-07 17:56:17 +0300

    1.5.0

--- wp-content/themes/swallow/functions.php
+++ wp-content/themes/swallow/functions.php
@@ -19,6 +19,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('blocks-button', get_template_directory_uri() . '/css/blocks/button.css');
     wp_enqueue_style('blocks-donate', get_template_directory_uri() . '/css/blocks/donate.css');
     wp_enqueue_style('blocks-ready-to-help', get_template_directory_uri() . '/css/blocks/ready-to-help.css');
+    wp_enqueue_style('blocks-select', get_template_directory_uri() . '/css/blocks/select.css');
     wp_enqueue_style('blocks-pagination', get_template_directory_uri() . '/css/blocks/pagination.css');
     wp_enqueue_style('blocks-partners', get_template_directory_uri() . '/css/blocks/partners.css');
     wp_enqueue_style('blocks-modal', get_template_directory_uri() . '/css/blocks/modal.css');
